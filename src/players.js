c89d8a27671ef35313307c3395251fe308e2a7b6
Acatalepsy <acatalepsy.meta@gmail.com>
2025-11-15 13:27:01 -0500

    no fetch?

--- src/players.js
+++ src/players.js
@@ -1,20 +1,16 @@
 
 async function fetch_all_players() {
-   fetch(request, env) {
     const result = await env.MY_DB.prepare(
         "SELECT discord_id, credits FROM [players]",
     ).run();
     return new Response(JSON.stringify(result));
-  }
 }
 
 async function add_new_player(discord_id) {
-	fetch(request, env) {
-    const result = await env.MY_DB.prepare(
-        `INSERT INTO players (discord_id, credits) values (${discord_id}, 100)`,
-    ).run();
-    return new Response(JSON.stringify(result));
-  }
+	const result = await env.MY_DB.prepare(
+		`INSERT INTO players (discord_id, credits) values (${discord_id}, 100)`,
+	).run();
+	return new Response(JSON.stringify(result));
 }
 
 module.exports = {
