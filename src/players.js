f8fee4672fb89c837def1e051a00f4d7471bf388
Acatalepsy <acatalepsy.meta@gmail.com>
2025-11-15 13:25:20 -0500

    awaiting

--- src/players.js
+++ src/players.js
@@ -1,6 +1,6 @@
 
-function fetch_all_players() {
-  async fetch(request, env) {
+async function fetch_all_players() {
+   fetch(request, env) {
     const result = await env.MY_DB.prepare(
         "SELECT discord_id, credits FROM [players]",
     ).run();
@@ -8,8 +8,8 @@ function fetch_all_players() {
   }
 }
 
-function add_new_player(discord_id) {
-  async fetch(request, env) {
+async function add_new_player(discord_id) {
+	fetch(request, env) {
     const result = await env.MY_DB.prepare(
         `INSERT INTO players (discord_id, credits) values (${discord_id}, 100)`,
     ).run();
